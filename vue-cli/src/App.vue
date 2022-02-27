dc6a218f5867954440bf411b3cc82d0cf982a65b
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-02-27 12:09:15 +0900

    App.vue와 HelloWorld.vue 설명

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -11,7 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    // 'hello-world': HelloWorld,
   }
 }
 </script>
